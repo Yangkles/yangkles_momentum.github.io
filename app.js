408f36f4992265bb4048f9dd2e5f3009922e2782
양윤호 <yang-yunho@yang-yunhos-MacBook-Pro.local>
2024-05-19 03:07:13 +0900

    carculator

---
+++ app.js
@@ -0,0 +1,24 @@
+const carculator = {
+    add : function(a,b){
+        alert(a+b);
+    },
+    minus : function(a,b){
+        alert(a-b);
+    },
+    multi : function(a,b){
+        alert(a*b);
+    },
+    divide : function(a,b){
+        alert(a/b);
+    },
+    square : function(a,b){
+        alert(a**b);
+    },
+};
+
+carculator.add(213123, 51351);
+carculator.minus(124313435, 4);
+carculator.multi(123, 6);
+carculator.divide(9,3);
+carculator.square(2,3);
+
