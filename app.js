27b3f8c52173b801ea6b2719561a367f7ddf136a
양윤호 <yang-yunho@yang-yunhos-MacBook-Pro.local>
2024-05-19 03:34:05 +0900

    return

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const age = 96;
-function calculateKrAge(ageOfForigner){
-    return ageOfForigner + 2;    
-}
-const krAge = calculateKrAge(age);
+const carculator = {
+    add : function(a,b){
+        return a+b;
+    },
+    minus : function(a,b){
+        return a-b;
+    },
+    multi : function(a,b){
+        return a*b;
+    },
+    divide : function(a,b){
+        return a/b;
+    },
+    square : function(a,b){
+        return a**b;
+    },
+};
 
-console.log(krAge);
+const plusResult = carculator.add(213123, 51351);
+const minusResult = carculator.minus(plusResult, 4);
+const multiplyResult = carculator.multi(123, minusResult);
+const divideResult = carculator.divide(minusResult,plusResult);
+const squareResult = carculator.square(divideResult,minusResult);
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(divideResult);
+console.log(multiplyResult);
+console.log(squareResult);
